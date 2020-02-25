d75017e108c1f9f7c9560ceb9d31830ffcd4b24e
Kevin Nguy <kevnguy@easypost.com>
2020-02-25 11:48:56 -0800

    Update go script

--- questions.go
+++ questions.go
@@ -18,18 +18,18 @@ func main() {
 	defer jsonFile.Close()
 
 	byteValue, _ := ioutil.ReadAll(jsonFile)
-	
+
 	var result []Question
 	json.Unmarshal(byteValue, &result)
 
-	var categories = make(map[string][]Question)
+	categories := make(map[string][]Question)
 	for _, question := range result {
-		var category = question.Category
+		category := question.Category
 		categories[category] = append(categories[category], question)
 	}
 
 	for category, questions := range categories {
-		var randomInt = rand.Intn(len(questions))
+		randomInt := rand.Intn(len(questions))
 		fmt.Println(randomInt, "Category:", category, "\nQuestion:", questions[randomInt].Question, "\n\n")
 	}
 }
